6d4ce884e2546191adbc1930dce1aae62e387841
Juani Melia <55026293+JuaniMelia@users.noreply.github.com>
2021-02-24 22:21:44 -0300

    Add files via upload

---
+++ js/main21.js
@@ -0,0 +1,21 @@
+function showmenu() {
+  document.getElementById("menuList").classList.toggle("show");
+}
+
+
+
+
+
+
+
+
+// addEventListener('DOMContentLoaded',() => {
+//   const btn-menu-21 = document.queryselector('.btn-menu-21')
+//     if (btn-menu-21) {
+//       btn-menu-21.addEventListener('click',() => {
+//         const menulist-21 = document.queryselector('.menulist-21')
+//         menulist-21.classlist.toggle('show')
+//       })
+//
+//     }
+// })
